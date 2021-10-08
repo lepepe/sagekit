3bdf9a3672b7532ef671baacb690df04fa8812b2
Jose Perez <jose.perez@vertilux.com>
2021-10-08 09:23:44 -0400

    Allow to create sales order with json payload

--- lib/sagekit/resources/sales_orders.rb
+++ lib/sagekit/resources/sales_orders.rb
@@ -9,32 +9,12 @@ module Sagekit
       SalesOrder.new get_request("#{company}/OE/OEOrders(#{order_id})", params: params).body
     end
 
-    # Create new record:
-    # body: json
-    # client.sales_orders.create(
-    #   "ACCLTD",
-    #   {
-    #    "CustomerNumber":"33016L",
-    #        "OrderReference":"26",
-    #        "OrderDescription":"Ship via: Delivery",
-    #        "OrderComment":"Ordered form ERP: 26. Testing UOM!",
-    #        "OrderDetails":
-    #        [
-    #            {
-    #               "Item":"0-005-39-00100",
-    #               "StockItem":true,
-    #               "QuantityOrdered":55.12
-    #            },
-    #            {
-    #               "Item":"0-005-19-00200",
-    #               "StockItem":true,
-    #               "QuantityOrdered":101.05
-    #            }
-    #        ]
-    #    }
-    #)
     def create(company, **attributes)
       SalesOrder.new post_request("#{company}/OE/OEOrders", body: attributes).body
+    end
+
+    def create_json(company, options={})
+      SalesOrder.new post_request("#{company}/OE/OEOrders", body: options).body
     end
   end
 end
